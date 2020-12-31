debad6713c2ed17c6a28a0d7ec64fea3821a1fec
bricsi0000000000000 <44650681+bricsi0000000000000@users.noreply.github.com>
2020-12-31 16:25:49 +0000

    Update gh-pages to output generated at f94d814

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['insertionsort',['InsertionSort',['../class_ricsi_1_1_insertion_sort.html',1,'Ricsi']]],
   ['iterator',['Iterator',['../struct_ricsi_1_1_iterator.html',1,'Ricsi']]]
 ];
