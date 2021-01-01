8332ac73553399ad2271ea37183f1d6e2b502708
bricsi0000000000000 <44650681+bricsi0000000000000@users.noreply.github.com>
2021-01-01 14:22:26 +0000

    Update gh-pages to output generated at d68cbda

--- search/all_9.js
+++ search/all_9.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['sort_5finsertion',['sort_insertion',['../class_ricsi_1_1_linked_list.html#aa51967fe8affc3d794711c5eb9587e5a',1,'Ricsi::LinkedList']]],
   ['sort_5fmerge',['sort_merge',['../class_ricsi_1_1_linked_list.html#af4527fa1872c6f1820be4d83b3aeaf82',1,'Ricsi::LinkedList']]],
-  ['swap',['swap',['../class_ricsi_1_1_linked_list.html#abf4cf35ab8d2a8475ad73d6d4c0bb567',1,'Ricsi::LinkedList']]]
+  ['sort_5fselection',['sort_selection',['../class_ricsi_1_1_linked_list.html#a3556905d9131242aaadb958d0cc7ee1a',1,'Ricsi::LinkedList']]],
+  ['swap',['swap',['../class_ricsi_1_1_linked_list.html#a96710e1b47d729ab9bba4ac483bd26a3',1,'Ricsi::LinkedList']]]
 ];
